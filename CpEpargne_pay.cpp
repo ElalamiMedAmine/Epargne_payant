112bf95d7c4bd7dc5b836128d10afbef52811e36
ElalamiMedAmine <94014119+ElalamiMedAmine@users.noreply.github.com>
2022-03-15 23:00:42 +0100

    Add files via upload

---
+++ CpEpargne_pay.cpp
@@ -0,0 +1,23 @@
+#include CpEpargne_pay.h
+
+CpEpargne_payCpEpargne_pay(Client c, Devise d, double taux)
+	CompteEpargne(c,d,taux), Compte(c,d);
+{
+}
+
+bool CpEpargne_payretirerArgent(Devise montant){
+
+
+
+}
+
+void CpEpargne_paydeposerArgent(Devise montant)
+ {
+
+
+ }
+
+
+CpEpargne_pay~CpEpargne_pay()
+{
+}
---
+++ CpEpargne_pay.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "CompteEpargne.h"
+#include "ComptePayant.h"
+#include "Devise.h"
+#include<iostream>
+using namespace std;
+class CpEpargne_pay : public CompteEpargne, public ComptePayant
+{
+
+public:
+	CpEpargne_pay(Client* c, Devise*d, double taux);
+	bool retirerArgent(Devise montant)override;
+	void deposerArgent(Devise montant)override;
+	~CpEpargne_pay();
+
+
+};
